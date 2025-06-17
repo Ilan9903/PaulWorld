dfb8647af82f2c917d9a9564979d63257676c3df
Ilan <galaxyforux@gmail.com>
2025-06-17 11:46:17 +0200

    maj pokemon mouvement

--- Assets/Script/PokemonController.cs
+++ Assets/Script/PokemonController.cs
@@ -3,6 +3,12 @@ using UnityEngine.UI; // Important pour utiliser le Slider
 
 public class PokemonController : MonoBehaviour
 {
+
+    public Vector2 zoneMin; // Coin inférieur gauche de la zone
+    public Vector2 zoneMax; // Coin supérieur droit de la zone
+    public float speed = 2f;
+    private Vector3 targetPosition;
+
     [Header("Health")]
     public float maxHealth = 100f;
     public Slider healthSlider; // Référence à notre barre de vie
@@ -10,6 +16,8 @@ public class PokemonController : MonoBehaviour
 
     void Start()
     {
+        SetNewRandomTarget();
+
         currentHealth = maxHealth;
         if(healthSlider != null) 
         {
@@ -17,11 +25,28 @@ public class PokemonController : MonoBehaviour
             healthSlider.value = currentHealth;
         }
     }
+    
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+        {
+            SetNewRandomTarget();
+        }
+    }
+
+    void SetNewRandomTarget()
+    {
+        float x = Random.Range(zoneMin.x, zoneMax.x);
+        float y = Random.Range(zoneMin.y, zoneMax.y);
+        targetPosition = new Vector3(x, y, transform.position.z);
+    }
 
     public void TakeDamage(float amount)
     {
         currentHealth -= amount;
-        if(healthSlider != null) 
+        if (healthSlider != null)
         {
             healthSlider.value = currentHealth;
         }
