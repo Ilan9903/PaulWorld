5225fa08089c822c03636e5afe320d5453c51f7d
Ilan <galaxyforux@gmail.com>
2025-06-18 10:30:51 +0200

    maj

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -37,7 +37,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
